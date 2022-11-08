8d7f88d5fe17c9bd5227fcbb1c3dabc47646a44c
bboyect <greateric0823@gmail.com>
2022-11-08 13:45:04 +0100

    Starting Assignment 3

---
+++ R/Assignment 3.R
@@ -0,0 +1,10 @@
+diabetes_data <- read.csv("D:/下載/pima-indians-diabetes.csv")
+colnames(diabetes_data) <- c("number_of_times_pregnant",
+                             "plasma_glucose_concentration", 
+                             "blood_pressure",
+                             "triceps_skinfold_thickness",
+                             "serum_insulin",
+                             "bmi",
+                             "diabetes_pedigree_function",
+                             "age",
+                             "diabetes")
