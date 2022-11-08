3afe6231c41c830a62b6225e44ecdba6c42b1193
bboyect <greateric0823@gmail.com>
2022-11-09 00:54:38 +0100

    finish Ex.1, Question about Ex.2: can we use "glm" function to compute logistic regression?

--- R/Assignment 3.R
+++ R/Assignment 3.R
@@ -1,3 +1,7 @@
+library(ggplot2)
+library(dplyr)
+
+#=====Import Data=====
 diabetes_data <- read.csv("D:/下載/pima-indians-diabetes.csv")
 colnames(diabetes_data) <- c("number_of_times_pregnant",
                              "plasma_glucose_concentration", 
@@ -8,3 +12,27 @@ colnames(diabetes_data) <- c("number_of_times_pregnant",
                              "diabetes_pedigree_function",
                              "age",
                              "diabetes")
+#=====EX.1=====
+plot_data <- diabetes_data
+plot_data$diabetes <- as.factor(plot_data$diabetes)
+ggplot(plot_data, aes(x=age, y=plasma_glucose_concentration,color=diabetes)) +
+    geom_point() + scale_color_manual(values=c("#000000", "#ff0000")) 
+
+#=====EX.2=====
+n=dim(diabetes_data)[1]
+set.seed(12345) 
+id <- sample(1:n, floor(n*0,8)) 
+train_data <-  diabetes_data[id,] 
+test_data <- diabetes_data[-id,] 
+
+model <- glm( diabetes ~plasma_glucose_concentration + age , data = train_data, family = binomial)
+summary(model)$coef
+probabilities <- model %>% predict(test_data, type = "response")#The type="response" option tells R to output probabilities of the form P(Y = 1|X), as opposed to other information such as the logit.
+predicted_classes <- ifelse(probabilities > 0.5, "1", "0")
+predicted_classes
+
+missclass=function(X,X1){
+  n=length(X)
+  return(1-sum(diag(table(X,X1)))/n)
+}
+missclass(as.factor(as.vector(test_data$diabetes)),predicted.classes)
