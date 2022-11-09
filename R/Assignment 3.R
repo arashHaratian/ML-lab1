f1ca39c437d176fb2fb0f60212db9fdf6fef139f
bboyect <greateric0823@gmail.com>
2022-11-09 01:35:02 +0100

    slight change about clasification

--- R/Assignment 3.R
+++ R/Assignment 3.R
@@ -21,12 +21,14 @@ ggplot(plot_data, aes(x=age, y=plasma_glucose_concentration,color=diabetes)) +
 #=====EX.2=====
 n=dim(diabetes_data)[1]
 set.seed(12345) 
-id <- sample(1:n, floor(n*0,8)) 
+id <- sample(1:n, floor(n*0.8)) 
 train_data <-  diabetes_data[id,] 
 test_data <- diabetes_data[-id,] 
 
 model <- glm( diabetes ~plasma_glucose_concentration + age , data = train_data, family = binomial)
+
 summary(model)$coef
+#p = exp(-5.77295229 +  0.03539974*plasma_glucose_concentration +0.02112258*age)/ [1 + exp(-5.77295229 +0.03539974*plasma_glucose_concentration +0.02112258*age)]
 probabilities <- model %>% predict(test_data, type = "response")#The type="response" option tells R to output probabilities of the form P(Y = 1|X), as opposed to other information such as the logit.
 predicted_classes <- ifelse(probabilities > 0.5, "1", "0")
 predicted_classes
@@ -35,4 +37,5 @@ missclass=function(X,X1){
   n=length(X)
   return(1-sum(diag(table(X,X1)))/n)
 }
-missclass(as.factor(as.vector(test_data$diabetes)),predicted.classes)
+missclass(as.factor(as.vector(test_data$diabetes)),as.numeric(predicted_classes))
+1-mean(predicted_classes == test_data$diabetes)
